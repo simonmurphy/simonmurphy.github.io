205e927123425b6c7cbe30ba7063d0ba76f06eb7
Simon Murphy <simon.murphy.nz@gmail.com>
2021-06-13 13:41:50 +1200

    Ready to go

--- js/header.js
+++ js/header.js
@@ -1,2 +1,3 @@
 d = new Date().getDay();
-document.getElementById("header").style.backgroundImage = "url(img/header"+d%4+".jpg)";
+// document.getElementById("header").style.backgroundImage = "url(img/header"+d%4+".jpg)";
+document.getElementById("header").style.backgroundImage = "url(img/header1.jpg)";
